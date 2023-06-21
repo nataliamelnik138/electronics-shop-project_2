deeb6d4be1695fb905cc93eed9f1f8e256991de6
Natalia Melnik <nataliamelnik138@gmail.com>
2023-06-21 14:43:05 +0600

    Implemented class and tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
+import pytest
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200_000
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
